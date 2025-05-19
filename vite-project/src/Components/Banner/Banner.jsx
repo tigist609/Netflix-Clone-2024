43281980a68731bee94b1f4863ec4d303baaf945
tigist609 <tegie88@yahoo.com>
2025-05-19 16:35:43 -0500

    axios and request

---
+++ vite-project/src/Components/Banner/Banner.jsx
@@ -0,0 +1,50 @@
+// import React, { useEffect, useState } from "react";
+// import axios from "../../Utils/axios";
+// import requests from "../../Utils/requests";
+// import "./Banner.css";
+
+// const Banner = () => {
+//   const [movie, setMovie] = useState({});
+//   useEffect(() => {
+//     (async () => {
+//       try {
+//         const request = await axios.get(requests.fetchNetflixOriginals);
+//         setMovie(
+//           request.data.results[
+//             Math.floor(Math.random() * request.data.results.length)
+//           ]
+//         );
+//       } catch (error) {
+//         console.log("error", error);
+//       }
+//     })();
+//   }, []);
+//   const truncate = (str, n) => {
+//     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+//   };
+//   return (
+//     <div
+//       className="banner"
+//       style={{
+//         backgroundSize: "cover",
+//         backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+//         backgroundPosition: "center",
+//         backgroundRepeat: "no-repeat",
+//       }}
+//     >
+//       <div className="banner_contents">
+//         <h1 className="banner_title">
+//           {movie?.title || movie?.name || movie?.original_name}
+//         </h1>
+//         <div className="banner_buttons">
+//           <button className="banner_button play">play</button>
+//           <button className="banner_button">My List</button>
+//         </div>
+//         <h1 className="banner_description">{truncate(movie?.overview, 150)}</h1>
+//       </div>
+//       <div className="banner_fadeBottom" />
+//     </div>
+//   );
+// };
+
+// export default Banner;
--- vite-project/src/pages/Home/Home.jsx
+++ vite-project/src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Header from "../../Components/Header/Header";
+import Banner from "../../Components/Banner/Banner";
+// import RowList from "../../Components/Rows/RowList/RowList";
 
 import Footer from "../../Components/Footer/Footer";
 const Home = () => {
   return (
     <>
       <Header />
-
+      <Banner />
+      {/* <RowList /> */}
       <Footer />
     </>
   );
